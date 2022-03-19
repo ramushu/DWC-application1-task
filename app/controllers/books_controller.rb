d82fe8c3b138248ba23bf603fa0ce8363f181c5d
EC2 Default User <ec2-user@ip-172-31-34-8.ec2.internal>
2022-03-19 13:26:14 +0000

    [update] update-error（CSS未調整）

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,9 +1,15 @@
 class BooksController < ApplicationController
 
   def create
-    book = Book.new(book_params)
-    book.save
-    redirect_to book_path(book.id)
+    @book = Book.new(book_params)
+    if @book.save
+      flash[:notice] = "#{@book.id} successfully"
+      redirect_to book_path(@book.id)
+    else
+      @books = Book.all
+      flash.now[:alert] = "error"
+      render :index
+    end
   end
 
   def index
@@ -20,9 +26,20 @@ class BooksController < ApplicationController
   end
 
   def update
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
+      flash[:notice] = "#{@book.id} successfully"
+      redirect_to book_path(@book.id)
+    else
+      flash.now[:alert] = "error"
+      render :edit
+    end
+  end
+
+  def destroy
     book = Book.find(params[:id])
-    book.update(book_params)
-    redirect_to book_path(book.id)
+    book.destroy
+    redirect_to "/books"
   end
 
   private
